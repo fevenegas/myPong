c1b3b2dc60053d994109554db1e2420ead0780ee
narvalis <66041153+fevenegas@users.noreply.github.com>
2023-03-12 16:25:13 -0300

    resolution work

--- main.lua
+++ main.lua
@@ -3,8 +3,8 @@
     Course:     GD50 2018
     Proyect:    Pong Remake
 
-    Version:    pong-0
-    "The Day-0 Update"
+    Version:    pong-1
+    ""
 
     -- Main Program --
 
@@ -25,26 +25,52 @@
     https://learning.edx.org/course/course-v1:HarvardX+CS50G+Games/home
 ]]
 
+-- push is a library that will allow us to draw our game at a virtual
+-- resolution, instead of however large our window is; used to provide
+-- a more retro aesthetic
+--
+-- https://github.com/Ulydev/push
+push = require 'push'
+
 -- Sets the size of the window view:
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 -- Runs when the game first starts up, only once; initializes the game.
 function love.load ()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text 
+    -- and graphics; try removing this function to see the difference!
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- initialize virtual resolution, it will be render within the actual window no matter its
+    -- dimensions; replaces the love.window.setMode call from the previous version.
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+-- Keyboard handling, called by LÖVE2D each frame; passes in the key pressed for access.
+function love.keypressed (key)
+    -- 'key' can be accessed by its string name
+    if key == 'escape' then
+        -- function LÖVE gives to terminate application
+        love.event.quit()
+    end
+end
+
 -- Called after update by LÖVE2D, used to draw on the screen, updated or otherwise.
 function love.draw ()
-    love.graphics.printf(
-        'Welcome to Pong!',     -- text to render
-        0,                      -- starting X (0 since we're heading to the center)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen)
-        'center'                -- alignment mode (center, left or right)
-    )
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+    -- condense versione (one line) from the last version, but now using the virtual resolution
+    love.graphics.printf('Welcom to Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
 end
